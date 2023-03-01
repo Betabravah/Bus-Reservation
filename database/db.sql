66a016dcca7ba064b322d94bccfd9615ef149bc4
betabravah <betabravah@gmail.com>
2023-03-01 05:11:19 +0300

    Added Databse

---
+++ database/db.sql
@@ -0,0 +1,56 @@
+drop database if EXISTS brs;
+
+create database brs;
+
+use brs;
+
+
+create table Bus (
+    id varchar(10) primary key,
+    capacity int not null        
+);
+
+create table User (
+    id varchar(10) primary key,
+    firstname varchar(50) not null,
+    middlename varchar(50) not null,
+    lastname varchar(50) not null,
+    `address` varchar(50),
+    phonenumber varchar(15),
+    `role` Enum('CUSTOMER', 'ADMINISTRATOR', 'DRIVER'),
+    `password` varchar(128) not null
+);
+
+create table Route (
+    id varchar(10) primary key,
+    source varchar(50) not null,
+    destination varchar(50) not null
+)
+
+
+create table ScheduledRoute (
+    id varchar(10) primary key,
+    busId varchar(10) not null,
+    routeId varchar(10) not null,
+    departureTime datetime not null,
+    arrivalTime datetime not null,
+
+    foreign key (busId) references Bus(id),
+    foreign key (routeId) references `Route`(id)
+
+);
+
+
+create table Reservation (
+    id varchar(10) primary key,
+    customerId varchar(10) not null,
+    busId varchar(10) not null,
+    scheduledRouteId varchar(10) not null,
+    seatNumber int not null,
+    purchaseDate datetime not null,
+
+    foreign key (customerId) references customer(id),
+    foreign key (busId) references bus(id),
+    foreign key (routeId) references ScheduledRoute(id)
+);
+
